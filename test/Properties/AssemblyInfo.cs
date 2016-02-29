8c39cc04ca84de397fa320ca5ded7ba3b8f24828
Chris Webb <chriswebb@users.noreply.github.com>
2016-02-29 05:10:36 -0600

    Update AssemblyInfo.cs

--- test/Properties/AssemblyInfo.cs
+++ test/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("csBCryptTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A test of an implementation the OpenBSD Blowfish password hashing algorithm")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Chris Webb")]
 [assembly: AssemblyProduct("csBCryptTest")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
